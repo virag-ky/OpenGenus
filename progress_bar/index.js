3f9b8aa6ce657cd78dbfb4fd9bcdc3655c41d63a
Virag Kormoczy <vkormoczy@gmail.com>
2023-02-09 21:49:49 +0200

    Add percentage

--- progress_bar/index.js
+++ progress_bar/index.js
@@ -4,26 +4,30 @@ const progressNumber = document.getElementById('progress-number');
 
 const runProgress = () => {
   let width = 0;
-
   const progressInterval = () => {
     // if there is a number value in the input, then run the progress until the given value
     if (progressNumber.value) {
-      if (width >= progressNumber.value) {
+      if (width >= 100 || width >= progressNumber.value) {
+        clearInterval(intervalId);
+        progressNumber.value = '';
+      } else {
+        width++;
+        progressBar.style.width = `${width}%`;
+        progressBar.innerText = `${width}%`;
+      }
+    } else {
+      // if there is no number value in the input, then just run the progress until 100%
+      if (width >= 100) {
         clearInterval(intervalId);
       } else {
         width++;
         progressBar.style.width = `${width}%`;
+        progressBar.innerText = `${width}%`;
       }
     }
-    // if there is no number value in the input, then just run the progress until 100%
-    if (width >= 100) {
-      clearInterval(intervalId);
-    } else {
-      width++;
-      progressBar.style.width = `${width}%`;
-    }
   };
-  let intervalId = setInterval(progressInterval, 30);
+
+  let intervalId = setInterval(progressInterval, 10);
 };
 
 runProgressButton.addEventListener('click', runProgress);
