456253ef894f5ec7ad25c3c564d69cd59e29d2e7
Virag Kormoczy <vkormoczy@gmail.com>
2023-02-09 20:47:24 +0200

    Set progress with number value

--- progress_bar/index.js
+++ progress_bar/index.js
@@ -1,13 +1,23 @@
 const progressBar = document.getElementById('progress-bar');
 const runProgressButton = document.getElementById('run-progress');
-let width = 1;
-let counter = 0;
+const progressNumber = document.getElementById('progress-number');
 
 const runProgress = () => {
+  let width = 0;
+
   const progressInterval = () => {
+    // if there is a number value in the input, then run the progress until the given value
+    if (progressNumber.value) {
+      if (width >= progressNumber.value) {
+        clearInterval(intervalId);
+      } else {
+        width++;
+        progressBar.style.width = `${width}%`;
+      }
+    }
+    // if there is no number value in the input, then just run the progress until 100%
     if (width >= 100) {
       clearInterval(intervalId);
-      counter = 0;
     } else {
       width++;
       progressBar.style.width = `${width}%`;
