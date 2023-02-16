3bbbb50cd00bb4db47c8a65b7a87cb9ff73eb208
Virag Kormoczy <vkormoczy@gmail.com>
2023-02-16 18:12:13 +0200

    Add ON/OFF functionality

--- calculator/index.js
+++ calculator/index.js
@@ -1,35 +1,8 @@
-const buttonsContainer = document.getElementById('buttons-container');
+import { active } from './modules/buttonsContainer.js';
 
-const buttonsTextContent = [
-  'On',
-  'AC',
-  'C',
-  '+',
-  '7',
-  '8',
-  '9',
-  '-',
-  '4',
-  '5',
-  '6',
-  'X',
-  '1',
-  '2',
-  '3',
-  '÷',
-  '0',
-  '.',
-  '=',
-];
+const currentNum = document.getElementById('current-number');
+const previousNum = document.getElementById('previous-number');
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operation');
 
-buttonsTextContent.forEach((button) => {
-  const btn = document.createElement('button');
-  btn.textContent = button;
-  btn.setAttribute('id', `${button}`);
-  buttonsContainer.appendChild(btn);
-});
-
-const switchBtn = document.getElementById('On');
-switchBtn.addEventListener('click', () => {
-  switchBtn.textContent === 'On' ? 'Off' : 'On';
-});
+console.log(operations);
