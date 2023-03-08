4e2ae532d56958c5756f2122cc9cc107e910174d
Virag Kormoczy <vkormoczy@gmail.com>
2023-03-08 15:18:26 +0200

    Create starter and finish row

--- maze/index.js
+++ maze/index.js
@@ -1,30 +1,25 @@
 const mazeContainer = document.getElementById('mazeContainer');
-
-const mazeMap = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1],
-  [1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1],
-  [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
-  [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-];
+const mazeMap = [];
 
 const createMaze = () => {
-  // Generate starter row (where we start) and the finish row (where the finish point is)
   const starterRow = [0];
   const finishRow = [0];
+  const topRow = [1];
+  const bottomRow = [1];
 
+  // Generate the starter row (where we start), the finish row (where the finish point is), the top and bottom row
   for (let x = 1; x < 12; x++) {
-    const randomNum = Math.round(Math.random());
+    const randomNum2 = Math.round(Math.random());
+    const randomNum3 = Math.round(Math.random());
     if (x === 11) {
       starterRow.push(1);
       finishRow.unshift(1);
     } else {
-      starterRow.push(randomNum);
-      finishRow.unshift(randomNum);
+      starterRow.push(randomNum2);
+      finishRow.unshift(randomNum3);
     }
+    topRow.push(1);
+    bottomRow.push(1);
   }
 
   // Append starter and finish row
@@ -32,6 +27,10 @@ const createMaze = () => {
   const randomIndex2 = Math.round(Math.random() * 7);
   mazeMap.splice(randomIndex1, 0, starterRow);
   mazeMap.splice(randomIndex2, 0, finishRow);
+
+  // Append top and bottom row
+  mazeMap.unshift(topRow);
+  mazeMap.push(bottomRow);
 
   // Create maze
   for (let i = 0; i < mazeMap.length; i++) {
