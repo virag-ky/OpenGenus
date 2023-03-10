d8008580dbb4c3c48d76fe26195feb06eda12a9e
Virag Kormoczy <vkormoczy@gmail.com>
2023-03-10 22:15:23 +0200

    Add current cell

--- maze/index.js
+++ maze/index.js
@@ -26,7 +26,7 @@ const rows = 800 / width;
 // Create index
 const index = (i, j) => {
   if (i < 0 || j < 0 || i > columns || j > rows - 1) {
-    return false;
+    return -1;
   }
   return i + j * columns;
 };
@@ -38,7 +38,7 @@ function Cell(i, j) {
   this.walls = [true, true, true, true];
   this.visited = false;
 
-  this.checkNeighbors = function () {
+  this.checkNeighbors = () => {
     const neighbors = [];
     const top = grid[index(i, j - 1)];
     const right = grid[index(i + 1, j)];
@@ -61,6 +61,8 @@ function Cell(i, j) {
     if (neighbors.length > 0) {
       const randomNum = Math.round(Math.random() * neighbors.length);
       return neighbors[randomNum];
+    } else {
+      return undefined;
     }
   };
 
@@ -72,7 +74,6 @@ function Cell(i, j) {
       ctx.beginPath();
       ctx.moveTo(x, y);
       ctx.lineTo(x + width, y);
-      ctx.fillStyle = 'rgb(250, 253, 175)';
       ctx.stroke();
       ctx.closePath();
     }
@@ -80,7 +81,6 @@ function Cell(i, j) {
       ctx.beginPath();
       ctx.moveTo(x + width, y);
       ctx.lineTo(x + width, y + width);
-      ctx.fillStyle = 'rgb(250, 253, 175)';
       ctx.stroke();
       ctx.closePath();
     }
@@ -88,7 +88,6 @@ function Cell(i, j) {
       ctx.beginPath();
       ctx.moveTo(x + width, y + width);
       ctx.lineTo(x, y + width);
-      ctx.fillStyle = 'rgb(250, 253, 175)';
       ctx.stroke();
       ctx.closePath();
     }
@@ -96,20 +95,32 @@ function Cell(i, j) {
       ctx.beginPath();
       ctx.moveTo(x, y + width);
       ctx.lineTo(x, y);
-      ctx.fillStyle = 'rgb(250, 253, 175)';
       ctx.stroke();
       ctx.closePath();
     }
 
-    if (this.visited) {
+    if (current.visited) {
       ctx.beginPath();
-      ctx.rect(x, y, width, width);
-      ctx.fillStyle = 'rgb(250, 150, 175)';
+      ctx.rect(current.i, current.j, width, width);
+      ctx.fillStyle = 'purple';
       ctx.fill();
       ctx.closePath();
     }
   };
 }
+
+const drawMaze = () => {
+  for (let z = 0; z < grid.length; z++) {
+    grid[z].show();
+  }
+
+  const next = current.checkNeighbors();
+
+  if (next) {
+    next.visited = true;
+    current = next;
+  }
+};
 
 const createCells = () => {
   for (let x = 0; x < rows; x++) {
@@ -119,21 +130,8 @@ const createCells = () => {
     }
   }
   current = grid[0];
+  current.visited = true;
+  drawMaze();
 };
 
 createCells();
-
-const drawMaze = () => {
-  for (let z = 0; z < grid.length; z++) {
-    grid[z].show();
-  }
-
-  current.visited = true;
-  const next = current.checkNeighbors();
-
-  if (next) {
-    next.visited = true;
-  }
-};
-
-drawMaze();
